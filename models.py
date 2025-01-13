7e7eb7cc54777e621a79dce43cc7a34a16384a10
Carl Yang <cayan@synaptics.com>
2025-01-13 18:11:53 +0800

    #1: Support QAT(Quantization Aware Training) model instead of PTQ(Post-Training Quantization).

--- models.py
+++ models.py
@@ -4,7 +4,7 @@
                       COMPANY CONFIDENTIAL
                        INTERNAL USE ONLY
 
- Copyright (C) 1997 - 2015  Synaptics Incorporated.  All right reserved.
+ Copyright (C) 1997 - 2024  Synaptics Incorporated.  All right reserved.
 
  This document contains information that is proprietary to Synaptics
  Incorporated. The holder of this document shall treat all information
@@ -22,34 +22,44 @@
 
 import numpy as np
 import tensorflow as tf
+import tensorflow_model_optimization as tfmot
 
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import regularizers
 from tensorflow.keras.models import Sequential
-from losses import pairwise_cross_entropy_alpha
+from nn_losses import pairwise_cross_entropy_alpha
 from tensorflow.keras.optimizers import Adam
 
-def get_dense_model_image():
-    model = Sequential()
-    model.add(tf.keras.Input(shape = (13, 13, 1)))
-    model.add(layers.Flatten())  # this converts our 3D feature maps to 1D feature vectors
-    model.add(layers.Dense(48, activation = 'relu'))
-    model.add(layers.Dense(24, activation = 'relu'))
-    model.add(layers.Dense(16, activation = 'relu'))
-    model.add(layers.Dense(1, activation = 'sigmoid'))
-        
-    model.compile(loss = "binary_crossentropy",
-                  optimizer = Adam(learning_rate = 1e-3), 
-                  metrics = ['accuracy'])
-    return model
 
-def get_model(mode):
-    alpha = np.array([[1, 1, 5], [1, 1, 5], [50, 50, 1]])
+def get_dense_model_image(clumpLength, fpadRxCnt):
+    model = Sequential()
+    if int(fpadRxCnt) > 0:
+      model.add(tf.keras.Input(shape = (1, int(clumpLength)*int(clumpLength)+int(fpadRxCnt), 1)))
+    else:
+      model.add(tf.keras.Input(shape = (int(clumpLength), int(clumpLength), 1)))
+    model.add(layers.Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    model.add(tfmot.quantization.keras.quantize_annotate_layer(layers.Dense(48, activation = 'relu')))
+    model.add(tfmot.quantization.keras.quantize_annotate_layer(layers.Dense(24, activation = 'relu')))
+    model.add(tfmot.quantization.keras.quantize_annotate_layer(layers.Dense(16, activation = 'relu')))
+    model.add(tfmot.quantization.keras.quantize_annotate_layer(layers.Dense(1, activation = 'sigmoid')))
+
+    quantize_model = tfmot.quantization.keras.quantize_apply(model)
+    
+    quantize_model.compile(loss = "binary_crossentropy",
+                  optimizer = Adam(learning_rate = 1e-3),
+                  metrics = ['accuracy'])
+    return quantize_model
+
+
+def get_model(mode, clumpLength, fpadRxCnt):
+    #alpha = np.array([[1, 1, 5], [1, 1, 5], [50, 50, 1]])
+
     if 'image' in mode:
         if 'dense' in mode:
-            model = get_dense_model_image()
+            model = get_dense_model_image(clumpLength, fpadRxCnt)
         else:
             print('CNN Model setting is incorrect, select from image_big, image_small, image_dense')
             exit(-1)
+
     return model
